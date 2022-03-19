9baddc9c06ba7541f84587cf13fa873a48bf523b
KTG\phansa <lekgaetonic>
2022-03-19 14:29:51 +0700

    revise page

--- lib/pages/home/page.dart
+++ lib/pages/home/page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -15,7 +16,7 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: const Color(0xFF161725),
           centerTitle: true,
-          title: const Text('Strumped'),
+          title: const Text('Deedzify'),
         ),
         body: FutureBuilder(
           future: readJson(),
@@ -28,14 +29,16 @@ class HomePage extends StatelessWidget {
                 itemBuilder: (BuildContext context, int index) {
                   return ListTile(
                     onTap: () => Get.to(const LyricPage()),
-                    leading: const Icon(Icons.list),
+                    leading: Image.network(songs['songs'][index]['cover']),
                     title: Text(
                       songs['songs'][index]['name'],
+                      style: const TextStyle(fontWeight: FontWeight.bold),
                     ),
                     subtitle: Text(
                       songs['songs'][index]['artist'],
                       style: const TextStyle(color: Colors.green, fontSize: 15),
                     ),
+                    trailing: Icon(CupertinoIcons.arrow_right),
                   );
                 },
               );

70f859210866764945028d6ee4227fa935c27edf
phansa <lekgaetonic@gmail.com>
2022-03-25 19:44:15 +0700

    s

--- lib/pages/home/body.dart
+++ lib/pages/home/body.dart
@@ -35,7 +35,8 @@ class HomeBody extends StatelessWidget {
                   children: [
                     Container(
                       // color: Colors.amber,
-                      padding: const EdgeInsets.all(8),
+                      padding:
+                          const EdgeInsets.only(left: 8, right: 8, bottom: 8),
                       width: 80,
                       child: ClipRRect(
                           borderRadius: BorderRadius.circular(8),
@@ -52,7 +53,8 @@ class HomeBody extends StatelessWidget {
                   children: [
                     Container(
                       // color: Colors.blue,
-                      padding: const EdgeInsets.all(8),
+                      padding:
+                          const EdgeInsets.only(left: 8, right: 8, bottom: 8),
                       width: 80,
                       child: ClipRRect(
                           borderRadius: BorderRadius.circular(8),
@@ -69,7 +71,8 @@ class HomeBody extends StatelessWidget {
                   children: [
                     Container(
                       // color: Colors.blue,
-                      padding: const EdgeInsets.all(8),
+                      padding:
+                          const EdgeInsets.only(left: 8, right: 8, bottom: 8),
                       width: 80,
                       child: ClipRRect(
                           borderRadius: BorderRadius.circular(8),
@@ -86,7 +89,8 @@ class HomeBody extends StatelessWidget {
                   children: [
                     Container(
                       // color: Colors.blue,
-                      padding: const EdgeInsets.all(8),
+                      padding:
+                          const EdgeInsets.only(left: 8, right: 8, bottom: 8),
                       width: 80,
                       child: ClipRRect(
                           borderRadius: BorderRadius.circular(8),
@@ -103,7 +107,8 @@ class HomeBody extends StatelessWidget {
                   children: [
                     Container(
                       // color: Colors.blue,
-                      padding: const EdgeInsets.all(8),
+                      padding:
+                          const EdgeInsets.only(left: 8, right: 8, bottom: 8),
                       width: 80,
                       child: ClipRRect(
                           borderRadius: BorderRadius.circular(8),
@@ -120,7 +125,8 @@ class HomeBody extends StatelessWidget {
                   children: [
                     Container(
                       // color: Colors.blue,
-                      padding: const EdgeInsets.all(8),
+                      padding:
+                          const EdgeInsets.only(left: 8, right: 8, bottom: 8),
                       width: 80,
                       child: ClipRRect(
                           borderRadius: BorderRadius.circular(8),
@@ -145,6 +151,122 @@ class HomeBody extends StatelessWidget {
                 fontSize: 18,
                 color: Colors.white,
               ),
+            ),
+          ),
+          SizedBox(
+            height: 108,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      // color: Colors.amber,
+                      padding:
+                          const EdgeInsets.only(left: 8, right: 8, bottom: 8),
+                      width: 80,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                              'https://musicstation.kapook.com/files_music2008/picture/0/3453.jpg')),
+                    ),
+                    const Text(
+                      'Potato',
+                      style: TextStyle(color: Colors.amber),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      // color: Colors.blue,
+                      padding:
+                          const EdgeInsets.only(left: 8, right: 8, bottom: 8),
+                      width: 80,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk-Bsxgr5gnAj-LAPcowmLLNx2BhIZpHvyxg&usqp=CAU')),
+                    ),
+                    const Text(
+                      '25 Hours',
+                      style: TextStyle(color: Colors.amber),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      // color: Colors.blue,
+                      padding:
+                          const EdgeInsets.only(left: 8, right: 8, bottom: 8),
+                      width: 80,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                              'https://s.isanook.com/jo/0/rp/r/w700/ya0xa0m1w0/aHR0cDovL2ltYWdlLmpvb3guY29tL0pPT1hjb3Zlci8wLzgxMDMwNTc3NDQwYjg0ZjQvMTAwMC5qcGc=.jpg')),
+                    ),
+                    const Text(
+                      'Clash',
+                      style: TextStyle(color: Colors.amber),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      // color: Colors.blue,
+                      padding:
+                          const EdgeInsets.only(left: 8, right: 8, bottom: 8),
+                      width: 80,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                              'https://s.isanook.com/jo/0/ui/484/2423305/dc6b8c0fd87d042da1c9432b4599cb20_1597220739.jpg')),
+                    ),
+                    const Text(
+                      'Body Slam',
+                      style: TextStyle(color: Colors.amber),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      // color: Colors.blue,
+                      padding:
+                          const EdgeInsets.only(left: 8, right: 8, bottom: 8),
+                      width: 80,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                              'https://is4-ssl.mzstatic.com/image/thumb/Music/v4/1d/01/27/1d0127cf-1233-3f43-337b-f77f1dcffc55/2294.jpg/400x400bb.jpg')),
+                    ),
+                    const Text(
+                      'Silly Fools',
+                      style: TextStyle(color: Colors.amber),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      // color: Colors.blue,
+                      padding:
+                          const EdgeInsets.only(left: 8, right: 8, bottom: 8),
+                      width: 80,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: Image.network(
+                              'https://upload.wikimedia.org/wikipedia/th/2/2e/%E0%B8%9B%E0%B8%81%E0%B8%AD%E0%B8%B1%E0%B8%A5%E0%B8%9A%E0%B8%B1%E0%B9%89%E0%B8%A1-Seven-BigAss.jpg')),
+                    ),
+                    const Text(
+                      'Big Ass',
+                      style: TextStyle(color: Colors.amber),
+                    )
+                  ],
+                ),
+              ],
             ),
           ),
         ],
